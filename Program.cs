44ca6c605e185d0508fa4aa8849cc1db6cdb3616
aoaodev <aoaodev@outlook.com>
2026-05-03 16:28:55 -0400

    Slow MS C# Playlist with docs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string aFriend = "Bill";
+Console.WriteLine($"Hello  {aFriend}");
+aFriend = "Maira";
+Console.WriteLine($"Hello  {aFriend}");
+
+string firstFriend = "Maria";
+string secondFriend = "Sage";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}.");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+string greeting = "      Hello world!     ";
+//greeting = greeting.Trim();
+Console.WriteLine($"{greeting.Trim()}");
+
+string[] Friends = [firstFriend, secondFriend];
+foreach (var f in Friends)
+{
+    Console.WriteLine(f);
+}
